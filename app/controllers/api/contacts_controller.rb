a8e0dc4b55e6eac89eacf9a7d6c70829469f0f85
Becca <rrdoukas@gmail.com>
2020-06-02 13:43:44 -0500

    updates REST

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,12 +1,47 @@
 class Api::ContactsController < ApplicationController
 
-  def print_contacts_action
-    @contact = Contact.first
-    render "print_contacts.json.jb"
+  def index
+    @contacts = Contact.all
+    render "index.json.jb"
   end 
 
-  def all_contacts_action
-    @contacts = Contact.all
-    render 'all_contacts.json.jb'
+  def show 
+    @contacts = Contact.find_by(id: params[:id])
+    render "show.json.jb"
   end 
+
+  def create
+    @contact = Contact.new(
+      id: params[:id],
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+    )
+
+    @contact.save
+    render "show.json.jb"
+  end 
+
+  def update 
+    @contact = Contact.find_by(id: params[:id])
+
+    @contact.id = params[:id] || @contact.id
+    @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.last_name = params[:last_name] || @contact.last_name
+    @contact.email = params[:email] || @contact.email
+    @contact.phone_number = params[:phone_number] || @contact.phone_number
+
+    @contact.save
+    render "show.json.jb"
+  end 
+  
+  def destroy
+    @contact = Contacts.find_by(id: params[:id])
+    
+    @contact.destroy
+    render json:{message: "The contact has been destroyed."}
+  end 
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/print_contacts_url" => "contacts#print_contacts_action"
-    get "/all_contacts_url" => "contacts#all_contacts_action"
+    get "/contacts" => "contacts#index"
+    get "/contacts/:id" => "contacts#show"
+    post "/contacts" => "contacts#create"
+    patch "/contacts/:id" => "contacts#update"
+    delete "/contacts/:id" => "contacts#destroy"
+
   end
 end
