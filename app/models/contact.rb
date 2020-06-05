3e18cabc4dac3907b605e7714dee04d112a44ded
Becca <rrdoukas@gmail.com>
2020-06-05 11:51:02 -0500

    adds email validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,8 +2,9 @@ class Contact < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
 
-  
+
   def friendly_updated_at
     updated_at.strftime("%b %e, %l:%M %p")
   end
