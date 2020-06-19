9346252098b9714fb466e030c60aefafafd72371
Becca <rrdoukas@gmail.com>
2020-06-19 11:04:08 -0500

    adds edit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,23 +6,58 @@ class ContactsController < ApplicationController
   end 
 
   def new 
+    render "new.html.org"
   end 
 
   def show
+    @contact = Contact.find(params[:id])
+    render "show.html.erb"
   end 
 
   def create 
+    @contact = Contact.new(
+      first_name: params[:first_name],
+      middle_name: params[:middle_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      groups: params[:groups],
+      bio: params[:bio],
+      latitude: params[:latitude],
+      longitude: params[:longitude]
+    )
+    @contact.save
+    redirect_to "/contacts/#{@contact.id}"
   end 
 
   def edit
-
+    @contact = Contact.find(params[:id])
+    render "edit.html.erb"
   end 
 
   def update 
+    @contact = Contact.find(params[:id])
+    @contact.first_name = params[:first_name]
+    @contact.middle_name = params[:middle_name]
+    @contact.last_name = params[:last_name]
+    # @contact.email = params[:email]
+    # @contact.groups = params[:groups]
+    # @contact.bio = params[:bio]
+    # @contact.latitude = params[:latitude]
+    # @contact.longitude = params[:longitude]
+    
+    
+    if @contact.save
+    redirect_to "/contacts/#{@contact.id}"
+    else
+      render json: {errors: @contact.errors.full_messages}, status: :unprocessable_entity
+    end
   end 
   
   
   def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    redirect_to "/contacts"
   end 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
-    get "/contacts" => "contacts#index"
-    get "/contacts/new" => "contacts#new"
-    post "/contacts" => "contacts#create"
-    get "/contacts/:id" => "contacts#show"
-  # get "/contacts/:id/edit" => "contacts#edit"
-  # patch "/contacts/:id" => "contacts#update"
-  # delete "/contacts/:id" => "contacts#destroy"
+  get "/contacts" => "contacts#index"
+  get "/contacts/new" => "contacts#new"
+  get "/contacts/:id" => "contacts#show"
+  post "/contacts" => "contacts#create"
+  get "/contacts/:id/edit" => "contacts#edit"
+
+  patch "/contacts/:id" => "contacts#update"
+  delete "/contacts/:id" => "contacts#destroy"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
